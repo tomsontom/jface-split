a3a4e660a8dad2b4b1c96ddee05617dbee85f029
tod <tod>
2004-04-20 19:15:32 +0000

    Fix for 59303

---
+++ src/org/eclipse/jface/progress/IElementCollector.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jface.progress;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+/**
+ * IElementCollector is a type that allows for the incremental
+ * update of a collection of objects. This interface is not
+ * intended to be implemented by other plug-ins.
+ * @deprecated Use org.eclipse.ui.progress.IElementCollector
+ * NOTE: This class will be deleted for 3.0
+ * @since 3.0
+ */
+public interface IElementCollector {
+
+	/**
+	 * Add element to the IElementCollector.
+	 * @param element
+	 * @param monitor
+	 */
+	public void add(Object element,IProgressMonitor monitor);
+
+	/**
+	 * Add elements to the IElementCollector.
+	 * @param element
+	 * @param monitor
+	 */
+	public void add(Object[] elements, IProgressMonitor monitor);
+	
+	/**
+	 * The element collection is done. Clean up any temporary
+	 * state.
+	 *
+	 */
+	public void done();
+
+}
